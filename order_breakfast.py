8af5e34ccb716259a85fe6e15e535d0f2f06b7eb
Michal Chacinski <mpchacinski@gmail.com>
2023-10-16 13:56:30 +0200

    refactoring: made code less repetitive

--- order_breakfast.py
+++ order_breakfast.py
@@ -1,50 +1,45 @@
 import time
 
 
+def print_pause(message_to_print):
+    print(message_to_print)
+    time.sleep(1)
+
+
 def ask_again():
     response = ""
 
     while "no" not in response and "yes" not in response:
         response = input("Would you like to place another order? Please say 'yes' or 'no'\n").lower()
         if "no" in response:
-            print("OK, goodbye!")
+            print_pause("OK, goodbye!")
         elif "yes" in response:
-            print("Very good, I'm happy to take another order.")
-            time.sleep(0.7)
+            print_pause("Very good, I'm happy to take another order.")
             ask_choice()
         else:
-            print("Sorry, I don't understand.")
+            print_pause("Sorry, I don't understand.")
 
 
 def ask_choice():
     response = ""
 
     while "waffles" not in response and "pancakes" not in response:
-        time.sleep(0.7)
         response = input("Please place your order. Would you like waffles or pancakes?\n").lower()
         if "waffles" in response:
-            print("Waffles it is!")
-            time.sleep(0.7)
-            print("Your order will be ready shortly.")
-            time.sleep(0.7)
+            print_pause("Waffles it is!")
+            print_pause("Your order will be ready shortly.")
             ask_again()
         elif "pancakes" in response:
-            print("Pancakes it is!")
-            time.sleep(0.7)
-            print("Your order will be ready shortly.")
-            time.sleep(0.7)
+            print_pause("Pancakes it is!")
+            print_pause("Your order will be ready shortly.")
             ask_again()
         else:
-            print("Sorry, I don't understand.")
+            print_pause("Sorry, I don't understand.")
 
 
-print("Hello! I am Bob, the Breakfast Bot.")
-time.sleep(0.7)
-print("Today we have two breakfasts available.")
-time.sleep(0.7)
-print("The first is waffles with strawberries and whipped cream.")
-time.sleep(0.7)
-print("The second is sweet potato pancakes with butter and syrup.")
+print_pause("Hello! I am Bob, the Breakfast Bot.")
+print_pause("Today we have two breakfasts available.")
+print_pause("The first is waffles with strawberries and whipped cream.")
+print_pause("The second is sweet potato pancakes with butter and syrup.")
 
 ask_choice()
-
