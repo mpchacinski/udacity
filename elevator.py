02a3fb1c068bef0fbaebbcc7c868d22e5e402052
Michal Chacinski <mpchacinski@gmail.com>
2023-10-16 17:33:11 +0200

    added logic to the third floor

--- elevator.py
+++ elevator.py
@@ -26,6 +26,7 @@ while True:
         else:
             print("The clerk greets you and gives you your ID card.")
             items.append("ID card")
+        print_pause("You head back to the elevator.")
     elif response == "2":
         print_pause("You push the button for the second floor.")
         print_pause("After a few moments, you find yourself in the human resources department.")
@@ -41,7 +42,25 @@ while True:
             else:
                 print_pause("He has something for you, but says he can't "
                             "give it to you until you go get your ID card.")
+        print_pause("You head back to the elevator.")
     elif response == "3":
         print_pause("You push the button for the third floor.")
         print_pause("After a few moments, you find yourself in the engineering department.")
+        print_pause("This is where you work!")
+        if "ID card" in items:
+            print_pause("You use your ID card to open the door.")
+            print_pause("Your program manager greets you and tells you that you need "
+                        "to have a copy of the employee handbook in order to start work.")
+            if "handbook" in items:
+                print_pause("Fortunately, you got that from HR!")
+                print_pause("Congratulatons! You are ready to start your new "
+                            "job as vice president of engineering!")
+                break
+            else:
+                print_pause("They scowl when they see that you don't have it, "
+                            "and send you back to the elevator.")
+        else:
+            print_pause("Unfortunately, the door is locked and you can't get in.")
+            print_pause("It looks like you need some kind of key card to open the door.")
+            print_pause("You head back to the elevator.")
     print_pause("Where would you like to go next?")
